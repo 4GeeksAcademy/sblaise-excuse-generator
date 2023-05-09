097ba9963d88a347c0b0f66b36fef428490b1c42
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-05-09 23:20:08 +0000

    ExcuseDone

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  /* document.querySelector('#btn').addEventListener("click", () => {});*/
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["My dog", "My Grandma", "His Turtle", "My bird"];
+  let action = ["ate", "peed", "crush", "broke"];
+  let what = ["my homework", "the key", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when i finished",
+    "during my break",
+    "while i was studying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
